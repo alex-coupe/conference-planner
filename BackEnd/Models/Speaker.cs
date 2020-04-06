0e75bff44854dd44c8421e5e46476601ccd9e59b
Alex <acooupe@gmail.com>
2020-04-06 10:49:06 +0100

    DTO project added and first one created

--- BackEnd/Models/Speaker.cs
+++ BackEnd/Models/Speaker.cs
@@ -2,18 +2,8 @@
 
 namespace BackEnd.Models
 {
-    public class Speaker
+    public class Speaker : ConferenceDTO.Speaker
     {
-        public int Id { get; set; }
-
-        [Required]
-        [StringLength(200)]
-        public string Name { get; set; }
-
-        [StringLength(4000)]
-        public string Bio { get; set; }
-
-        [StringLength(1000)]
-        public virtual string Website { get; set; }
+       
     }
 }
